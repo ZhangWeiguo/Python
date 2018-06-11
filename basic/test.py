688b18e1a357a8fd3721837890ebe1dc2243fac9
wb-zwg306321 <wb-zwg306321@alibaba-inc.com>
2018-06-11 17:07:38 +0800

    test rm

--- basic/test.py
+++ basic/test.py
@@ -1,23 +1,17 @@
-import time
-from logger import LogClient,LogServer
+import multiprocessing,time,os
+from logger import Logger
 
-def test1():
-    import time
-    logger = LogClient("Test","test", rotate = "Time", when = 'M', keep_num = 48)
+def test(process_name):
+    logger = Logger("Test","test", rotate = "Time", when = 'M', keep_num = 5)
+    k = 0
+    pid = os.getpid()
     while True:
-        logger.error("zhangweiuoerror")
-        logger.info("zhangweiuoinfo")
-        logger.debug("zhangweiuodebug")
-        logger.warning("zhangweiuowarning")
-        logger.fatal("zhangweiuofatal")
-        time.sleep(10)
-
-def test2():
-    logger = LogServer(app_name = 'test', keep_num = 24, buffer_num = 100)
-    while True:
-        logger.log(time.strftime("%Y%m%d %H:%M:%S") + "zhangweiuoinfo")
-        time.sleep(0.1)
-
+        k += 1
+        message = "%3d %6d %s"%(k, pid, process_name)
+        logger.info(message)
+        time.sleep(1)
 if __name__ == "__main__":
-    test2()
+    q1 = multiprocessing.Process(target=test, kwargs={"process_name":"process one"})
+    q1.start()
+
 
