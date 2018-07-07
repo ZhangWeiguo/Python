2564d4eb7073be1cb705236fd65ed583fa1d2b80
wb-zwg306321 <wb-zwg306321@alibaba-inc.com>
2018-07-07 17:42:24 +0800

    selfie server

--- services/selfie/start-uwsgi.sh
+++ services/selfie/start-uwsgi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-appname=`cat app.ini | grep "procname-master" | awk '{print $3}'`
+appname="selfie-uwsgi.ini"
 echo "AppName : $appname"
 pids=`ps -ef | grep $appname | grep -v "grep" | awk '{print $2}'`
 if [ -n "$pids" ]
--- services/selfie/stop-uwsgi.sh
+++ services/selfie/stop-uwsgi.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# function killapp()
-# {
-# 	appname=$1
-# 	pids=`ps -ef | grep $appname | grep -v "grep" | awk '{print $2}'`
-# 	for pid in $pids
-# 	do
-# 		kill -s 9 $pid
-# 		echo "Killed $pid"
-# 	done
-# }
-# appname=`cat app.ini | grep "procname-master" | awk '{print $3}'`
-# killapp $appname
-# echo "App Is Stopped!"
-uwsgi --stop log/uwsgi.pid
+appname="selfie-uwsgi.ini"
+function killapp()
+{
+	appname=$1
+	pids=`ps -ef | grep $appname | grep -v "grep" | awk '{print $2}'`
+	for pid in $pids
+	do
+		kill -s 9 $pid
+		echo "Killed $pid"
+	done
+}
+uwsgi --stop log/uwsgi.pid || killapp $appname
+echo "App Is Stopped!"
