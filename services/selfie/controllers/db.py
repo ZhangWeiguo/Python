3bef62135904181c60cb41f1336695ea9a2e0fc1
wb-zwg306321 <wb-zwg306321@alibaba-inc.com>
2018-07-14 11:52:38 +0800

    Login Page

--- services/selfie/controllers/db.py
+++ services/selfie/controllers/db.py
@@ -7,7 +7,7 @@ from init import logger,mysql_client
 
 
 def check_user(user_name, pass_word):
-    sql = 'select * from user_info where user=%s and password=%s'%(user_name, pass_word)
+    sql = "select * from user_info where user_name='%s' and pass_word='%s'"%(user_name, pass_word)
     result = mysql_client.query(sql)
     if result['succ'] == True:
         n = len(result['data'])
