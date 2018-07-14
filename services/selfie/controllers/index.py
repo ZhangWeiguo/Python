348346649a8cc4ce9a68105f0ecf82530bf226e5
wb-zwg306321 <wb-zwg306321@alibaba-inc.com>
2018-07-14 12:43:01 +0800

    Login Page

--- services/selfie/controllers/index.py
+++ services/selfie/controllers/index.py
@@ -5,10 +5,24 @@ import web,os
 
 class Index:
     def GET(self):
+        try:
+            user_name = web.config.session["user_name"]
+            user_page = "/%s"%(user_name)
+        except:
+            user_name = "登录"
+            user_page = "/login"
+        data = {"user_name":user_name,"user_page":user_page}
         s = os.path.join('templates','index.html')
         render = web.template.frender(s)
-        return render()
+        return render(data)
     def POST(self):
+        try:
+            user_name = web.config.session["user_name"]
+            user_page = "/%s"%(user_name)
+        except:
+            user_name = "登录"
+            user_page = "/login"
+        data = {"user_name":user_name,"user_page":user_page}
         s = os.path.join('templates','index.html')
         render = web.template.frender(s)
-        return render()
+        return render(data)
