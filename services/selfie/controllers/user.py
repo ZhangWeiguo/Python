794557efa4d38ab16e505fcfc4fcdb4454f1d5ca
wb-zwg306321 <wb-zwg306321@alibaba-inc.com>
2018-07-27 16:01:56 +0800

    User Home Page

--- services/selfie/controllers/user.py
+++ services/selfie/controllers/user.py
@@ -2,13 +2,32 @@
 # created by zwg in 20180617
 import web,os,sys
 sys.path.append("..")
-from init import logger
+from init import logger,global_data
 from utils import get_template_path
 from db import check_user
 
 class User:
     def GET(self, user_name):
         logger.info("%s Get The User Page"%web.cookies().get("session_id"))
+
+        try:
+            user_name = web.config.session["user_name"]
+            user_page = "/user/%s"%(user_name)
+        except:
+            user_name = "登录"
+            user_page = "/login"
+        if user_name == "":
+            user_name = "登录"
+            user_page = "/login"
+        user_data = {"user_name":user_name,"user_page":user_page}
+        cate_data = []
+        path = get_template_path("cate.html")
+        render = web.template.frender(path)
+        blog_cate = global_data["html_data"]["blog_cate"].keys()
+        for one in blog_cate:
+            cate_data.append({"name":one,"url":"/cate/%s"%one})
+        
+
         path = get_template_path("user.html")
         render = web.template.frender(path)
-        return render()
+        return render(user_data, cate_data)
